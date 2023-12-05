d113410ca7cf6918d9319ef9ff7d0c24c5ab1165
Zach Davis <zachdavis0099@gmail.com>
2023-12-04 19:09:53 -0600

    Prepare for Heroku deployment

--- app.py
+++ app.py
@@ -3,26 +3,43 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-api_key = '3mM44UdBKK8qxC_Pa88SUFi3ixzAVUwgKsPqF'
-api_secret = 'LKUPF4B2Y6kzXXfuGZLmwg'
-
 @app.route('/check_domains', methods=['POST'])
 def check_domains():
     data = request.json
     domain_names = data['domain_names']
-    available_domains = []
+    available_domains_info = []
 
     for domain in domain_names:
-        url = f"https://api.ote-godaddy.com/v1/domains/available?domain={domain}"
+        url = f"https://api.godaddy.com/v1/domains/available?domain={domain}"
         headers = {"Authorization": f"sso-key {api_key}:{api_secret}"}
         response = requests.get(url, headers=headers)
         
+        # if response.status_code == 200:
+        #     domain_data = response.json()
+        #     if domain_data['available']:
+        #         purchase_url = f'https://www.godaddy.com/domainsearch/find?domainToCheck={domain}'
+        #         available_domains_info.append({
+        #             "domain": domain,
+        #             "price": domain_data['price'],
+        #             "currency": domain_data['currency'],
+        #             "purchase_url": purchase_url
+        #         })
+
         if response.status_code == 200:
-            data = response.json()
-            if data['available']:
-                available_domains.append(domain)
+            domain_data = response.json()
+            if domain_data['available']:
+                purchase_url = f'https://www.godaddy.com/domainsearch/find?domainToCheck={domain}'
+                formatted_price = domain_data['price'] / 1000000  # Adjust the price format
+                
+                available_domains_info.append({
+                "domain": domain,
+                "price": formatted_price,
+                "currency": domain_data['currency'],
+                "purchase_url": purchase_url
+                })
         
-    return jsonify({"available_domains": available_domains})
+    return jsonify({"available_domains": available_domains_info})
 
 if __name__ == "__main__":
     app.run(debug=True)
+
